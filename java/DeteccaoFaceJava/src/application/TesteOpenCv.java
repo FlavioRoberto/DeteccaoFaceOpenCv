cc35b74c6d0ad4a42179ddf1644ad91dc6582039
Flávio Roberto Teixeira <flaviolive3@hotmail.com>
2018-04-29 13:38:44 -0300

    Convertendo matriz Mat em uma imagem

--- java/DeteccaoFaceJava/src/application/TesteOpenCv.java
+++ java/DeteccaoFaceJava/src/application/TesteOpenCv.java
@@ -1,12 +1,20 @@
-
 package application;
 
 import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+import util.GeradorDeIMagem;
 
 public class TesteOpenCv {
+
     public static void main(String[] args) {
-      //adicionando biblioteca do OpenCv
-      System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-      System.out.println(Core.VERSION);
+        //adicionando biblioteca do OpenCv
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        System.out.println(Core.VERSION);
+
+        //Convertendo a imagem para uma matriz numérica
+        Mat imagemColorida = Imgcodecs.imread("src\\img\\opencv.jpg", Imgcodecs.CV_LOAD_IMAGE_COLOR);
+        GeradorDeIMagem gerador = new GeradorDeIMagem(imagemColorida);
+        gerador.Constroi();
     }
 }
---
+++ java/DeteccaoFaceJava/src/util/GeradorDeIMagem.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import java.awt.FlowLayout;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import org.opencv.core.Mat;
+
+/**
+ *
+ * @author Admin
+ */
+public class GeradorDeIMagem {
+    private Mat matrizImagem;
+
+
+    public GeradorDeIMagem(Mat matrizImagem) {
+        this.matrizImagem = matrizImagem;
+    }
+
+    private BufferedImage ConverteMatParaImagem(){
+        if(matrizImagem == null)
+            return null;
+        int type = BufferedImage.TYPE_BYTE_GRAY;
+        if (matrizImagem.channels() > 1) {
+            type = BufferedImage.TYPE_3BYTE_BGR;
+        }
+
+        int bufferSize = matrizImagem.channels() * matrizImagem.cols() * matrizImagem.rows();
+        byte[] bytes = new byte[bufferSize];
+        matrizImagem.get(0, 0, bytes);
+        BufferedImage imagem = new BufferedImage(matrizImagem.cols(), matrizImagem.rows(), type);
+        byte[] targetPixels = ((DataBufferByte) imagem.getRaster().getDataBuffer()).getData();
+        System.arraycopy(bytes, 0, targetPixels, 0, bytes.length);
+        return imagem;
+    }
+    
+    public void Constroi(){
+        BufferedImage imagemConvertida = ConverteMatParaImagem();
+        if(imagemConvertida == null)
+            return;
+        ImageIcon icon = new ImageIcon(imagemConvertida);
+        JFrame frame = new JFrame();
+        frame.setLayout(new FlowLayout());
+        frame.setSize(imagemConvertida.getWidth() + 50, imagemConvertida.getHeight() + 50);
+        JLabel lbl = new JLabel();
+        lbl.setIcon(icon);
+        frame.add(lbl);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+}
